4ec4a689b5ca1b54b2b37f52dbd83cc6418925f7
Eugênio Rodrigues <eugenio28.04@gmail.com>
2023-10-09 21:42:13 -0300

    adições no codigo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import "./home.css";
 
 const APIPersonagens = "https://rickandmortyapi.com/api/character";
 const APIPesquisarPersonagem = "https://rickandmortyapi.com/api/character/?name="; //colocar o nome do personagem pesquisado
@@ -13,36 +14,47 @@ const APIPesquisarPersonagem = "https://rickandmortyapi.com/api/character/?name=
 
 export default function Home() {
     const [personagens, setPersonagens] = useState([]);
-    const [numPages] = useState(42);
+    const numPages = 42
 
     const buscarPersonagens = async () => {
         let todosOsDados = [];
 
-        for (let i = 0; i < numPages; i++) {
+        for (let i = 1; i < numPages; i++) {
             await axios.get(`${APIPersonagens}?page=${i}`)
                 .then(({ data }) => {
-                    setPersonagens(prevState => {
-                        return { ...prevState, data };
-                    });
+                    todosOsDados = [...todosOsDados, ...data.results]
                 }, err => {
                     console.error("Personagens não encontrados", err);
                 });
         }
+
+        setPersonagens(todosOsDados);
     }
 
     useEffect(() => {
         buscarPersonagens();
-        console.log(personagens);
     }, []);
 
     return (
-        <div>
-            <h1>Lista de Personagens</h1>
+        <div className="container">
+            <h1 className="titulo">Lista de Personagens</h1>
 
             {
-                personagens && (
-                    <ul>
+                personagens && personagens.length > 0 && (
+                    <ul className="lista-de-personagens">
+                        {
+                            personagens.map((personagem, i) => {
+                                return (
+                                    <li key={i}>
+                                        <Link className="personagem">
+                                            <img src={personagem.image} className="foto" />
 
+                                            <p className="nome">{personagem.name}</p>
+                                        </Link>
+                                    </li>
+                                )
+                            })
+                        }
                     </ul>
                 )
             }
